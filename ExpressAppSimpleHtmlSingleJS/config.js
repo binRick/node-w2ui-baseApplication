bba5c70f564b09875f930c6782d501a9560d933f
binRick <rapidvps@gmail.com>
2015-06-18 20:18:24 -0400

    new

---
+++ ExpressAppSimpleHtmlSingleJS/config.js
@@ -0,0 +1,9 @@
+ exports.rethinkHost = '127.0.0.1';
+ exports.rethinkPort = 28015;
+ exports.rethinkAuthKey = '';
+ exports.rethinkDB = 'wines';
+
+
+ exports.expressPort = 8989;
+ exports.expressHost = '0.0.0.0';
+ exports.debug = true;
